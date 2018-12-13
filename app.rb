37b0e1074fe2528c0444a47871e5b6a4ed608617
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-13 16:35:01 +0000

    Players can now enter names

--- app.rb
+++ app.rb
@@ -1,17 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb:play
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
+  get '/play' do
+       erb :play, locals: {
+       player_1: session[:player_1_name],
+       player_2: session[:player_2_name],
+    }
+  end
   run! if app_file == $0
 
 end
